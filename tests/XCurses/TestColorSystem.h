2a2446a27b98a5c7086995b20068f89787948eb6
Artem <AsharnRus@mail.ru>
2019-02-07 00:53:35 +0300

    Add xcurses color test

---
+++ tests/XCurses/TestColorSystem.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <XCurses/ColorPalette.h>
+#include <XCurses/ColorSystem.h>
+#include <XCurses/Color.h>
+#include <XCurses/Char.h>
+
+using namespace xcur;
+
+void test_ColorSystem()
+{
+	ColorSystem colorSystem;
+	Char ch = 'J';
+    
+}
--- tests/XCurses/main.cpp
+++ tests/XCurses/main.cpp
@@ -1,5 +1,11 @@
 #include <XCurses/Curses.h>
+#include "TestColorSystem.h"
+
+using namespace xcur;
 
 void main() {
+	Curses curs;
+	curs.init();
 
+	test_ColorSystem();
 }
