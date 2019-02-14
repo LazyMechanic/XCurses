1e6fd9456096119f32bd508008ff463ec54b6f60
Artem <AsharnRus@mail.ru>
2019-02-14 21:39:53 +0300

    Add Input class

--- include/XCurses/Input.h
+++ include/XCurses/Input.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <XCurses/Window.h>
+
 namespace xcur {
+class Char;
+
 class Input
 {
 public:
+	static void handleEvents(const Window::Ptr<>& window);
+
+	static Char getPressedKey();
 
 private:
-
+	static Char m_pressedChar;
 };
 }
--- src/XCurses/Input.cpp
+++ src/XCurses/Input.cpp
@@ -1,5 +1,24 @@
 #include <XCurses/Input.h>
 
-namespace xcur {
+#include <XCurses/Char.h>
 
+#include <PDCurses/curses.h>
+
+namespace xcur {
+Char Input::m_pressedChar(ERR);
+
+void Input::handleEvents(const Window::Ptr<>& window)
+{
+	if (window == nullptr) {
+		m_pressedChar = Char::Err;
+		return;
+	}
+
+	m_pressedChar = static_cast<uint16_t>(wgetch(window->getCursesWin()));
+}
+
+Char Input::getPressedKey()
+{
+	return m_pressedChar;
+}
 }
