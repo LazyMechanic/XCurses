91e7f91309e7b4295c096c145d2f0398cbfd7add
Artem <AsharnRus@mail.ru>
2019-02-17 19:20:06 +0300

    Add RootObject class

---
+++ include/XCurses/RootObject.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <XCurses/Drawable.h>
+#include <XCurses/Behaviour.h>
+#include <XCurses/ContextComponent.h>
+
+namespace xcur {
+class RootObject : virtual public ContextComponent
+{
+public:
+	virtual ~RootObject() = default;
+
+	/**
+	 * \brief Call for update object state
+	 * \param dt Delta time
+	 */
+    virtual void update(float dt) override = 0;
+
+	/**
+	 * \brief Call function for draw the object
+	 */
+    virtual void draw() override = 0;
+
+protected:
+    /**
+	 * \brief Default root constructor
+	 */
+	RootObject() = default;
+};
+}
