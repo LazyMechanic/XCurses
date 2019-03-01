a27415e05c896c4660b995841ac35be1dd6ea9f1
Artem <AsharnRus@mail.ru>
2019-03-01 16:30:12 +0300

    Update doxygen

---
+++ doc/mainpage.h
@@ -0,0 +1,10 @@
+/**
+ * \mainpage
+ *
+ * \section welcome Welcome
+ * Welcome to the XCurses documentation. Here you will find a detailed
+ * view of all the XCurses <a href="./annotated.html">classes</a> and functions. <br/>
+ *
+ * \section example Short example
+ * Here is a short example, to show you how simple it is to use XCurses: //TODO
+ */
