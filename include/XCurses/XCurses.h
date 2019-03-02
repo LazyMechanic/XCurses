30f2d100677cb18f44cf064d0e010cbfcefd5d4e
Artem <AsharnRus@mail.ru>
2019-03-02 23:42:24 +0300

    Update main XCurses.h

--- include/XCurses/XCurses.h
+++ include/XCurses/XCurses.h
@@ -1,23 +1,26 @@
 #pragma once
 
-#include <XCurses/System/Core.h>
-#include <XCurses/Text/Char.h>
 #include <XCurses/Graphics/Color.h>
-#include <XCurses/System/Input.h>
 #include <XCurses/Graphics/Object.h>
-#include <XCurses/System/Status.h>
 #include <XCurses/Graphics/Widget.h>
 #include <XCurses/Graphics/Window.h>
 #include <XCurses/Graphics/Border.h>
-#include <XCurses/System/Vector.h>
 #include <XCurses/Graphics/Context.h>
 #include <XCurses/Graphics/Drawable.h>
-#include <XCurses/Text/Attribute.h>
 #include <XCurses/Graphics/Behaviour.h>
 #include <XCurses/Graphics/Container.h>
 #include <XCurses/Graphics/HasWindow.h>
-#include <XCurses/System/CoreConfig.h>
 #include <XCurses/Graphics/ColorSystem.h>
 #include <XCurses/Graphics/ColorPalette.h>
 #include <XCurses/Graphics/ContextSystem.h>
 #include <XCurses/Graphics/ContextComponent.h>
+
+#include <XCurses/System/Core.h>
+#include <XCurses/System/Input.h>
+#include <XCurses/System/Status.h>
+#include <XCurses/System/Vector.h>
+#include <XCurses/System/CoreConfig.h>
+
+#include <XCurses/Text/Char.h>
+#include <XCurses/Text/String.h>
+#include <XCurses/Text/Attribute.h>
