98632f204e6dcf48689800fd535bc490bc03e1d2
Artem <AsharnRus@mail.ru>
2019-03-03 22:23:44 +0300

    Add tests of Attribute

--- test/XCurses/TestText.cpp
+++ test/XCurses/TestText.cpp
@@ -20,6 +20,110 @@ TEST_CASE("Attribute init and edit", "[String]")
 		REQUIRE(attr.toCursesAttr() == 0x00080000);
 		REQUIRE(attr.has(Attribute::Italic) == true);
     }
+
+	SECTION("Operators")
+	{
+		SECTION("Attribute::operator=(uint32_t attr)")
+		{
+			Attribute attr;
+			attr = 0x00650000;
+			REQUIRE(static_cast<uint32_t>(attr.value) == 0x65);
+		}
+
+        SECTION("Attribute::operator==(const Attribute& right)")
+		{
+			Attribute attr1(0x00080000);
+			Attribute attr2(0x00080000);
+			REQUIRE(attr1 == attr2);
+		}
+
+		SECTION("Attribute::operator==(uint32_t right)")
+		{
+			Attribute attr(0x00080000);
+			REQUIRE(attr == 0x00080000);
+		}
+
+		SECTION("Attribute::operator!=(const Attribute& right)")
+		{
+			Attribute attr1(0x00080000);
+			Attribute attr2(0x00070000);
+			REQUIRE(attr1 != attr2);
+		}
+
+		SECTION("Attribute::operator!=(uint32_t right)")
+		{
+			Attribute attr(0x00080000);
+			REQUIRE(attr != 0x00070000);
+		}
+
+		SECTION("Attribute::operator|(const Attribute& right)")
+		{
+			Attribute attr1(0x00080000);
+			Attribute attr2(0x00320000);
+			Attribute resultAttr = attr1 | attr2;
+
+			REQUIRE(resultAttr == 0x003a0000);
+		}
+
+		SECTION("Attribute::operator|(uint32_t right)")
+		{
+			Attribute attr(0x00080000);
+			Attribute resultAttr = attr | 0x00320000;
+
+			REQUIRE(resultAttr == 0x003a0000);
+		}
+
+		SECTION("Attribute::operator&(const Attribute& right)")
+		{
+			Attribute attr1(0x00250000);
+			Attribute attr2(0x00320000);
+			Attribute resultAttr = attr1 & attr2;
+
+			REQUIRE(resultAttr == 0x00200000);
+		}
+
+		SECTION("Attribute::operator&(uint32_t right)")
+		{
+			Attribute attr(0x00250000);
+			Attribute resultAttr = attr & 0x00320000;
+
+			REQUIRE(resultAttr == 0x00200000);
+		}
+
+		SECTION("Attribute::operator|=(const Attribute& right)")
+		{
+			Attribute attr1(0x00080000);
+			Attribute attr2(0x00320000);
+			attr1 |= attr2;
+
+			REQUIRE(attr1 == 0x003a0000);
+		}
+
+		SECTION("Attribute::operator|=(uint32_t right)")
+		{
+			Attribute attr(0x00080000);
+			attr |= 0x00320000;
+
+			REQUIRE(attr == 0x003a0000);
+		}
+
+		SECTION("Attribute::operator&=(const Attribute& right)")
+		{
+			Attribute attr1(0x00250000);
+			Attribute attr2(0x00320000);
+			attr1 &= attr2;
+
+			REQUIRE(attr1 == 0x00200000);
+		}
+
+		SECTION("Attribute::operator&=(uint32_t right)")
+		{
+			Attribute attr(0x00250000);
+			attr &= 0x00320000;
+
+			REQUIRE(attr == 0x00200000);
+		}
+	}
 }
 
 TEST_CASE("Char init and edit", "[Char]")
@@ -54,6 +158,16 @@ TEST_CASE("Char init and edit", "[Char]")
 		REQUIRE(static_cast<uint32_t>(ch.getColorPairId()) == 10);
 		REQUIRE(static_cast<uint32_t>(ch.getChar()) == 25);
 		REQUIRE(ch.toCursesChar() == 0x0A800019);
+	}
+
+    SECTION("Operators")
+	{
+	    SECTION("Char::operator=(uint16_t ch)")
+	    {
+			Char ch;
+			ch = 254;
+			REQUIRE(static_cast<uint32_t>(ch.getChar()) == 254);
+	    }
 	}
 }
 
