c6954099a5439ca3daf1c189ed5453100e02b8b4
LazyMechanic <AsharnRus@mail.ru>
2019-03-12 00:18:07 +0300

    Add operators for String

--- include/XCurses/Text/String.h
+++ include/XCurses/Text/String.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include <string>
 
 #include <XCurses/Text/Char.h>
@@ -12,12 +11,12 @@ public:
     /**
      * \brief std::vector<Char>::iterator alias
      */
-    using Iterator = std::vector<Char>::iterator;
+    using Iterator = std::basic_string<Char>::iterator;
 
 	/**
 	 * \brief std::vector<Char>::const_iterator alias
 	 */
-    using ConstIterator = std::vector<Char>::const_iterator;
+    using ConstIterator = std::basic_string<Char>::const_iterator;
    
     /**
      * \brief String constructor. Construct String from std::string
@@ -30,6 +29,37 @@ public:
      * \param str Wide string
      */
     String(const std::wstring& str);
+
+	/**
+	 * \brief String constructor. Construct String from wchar_t*
+	 * \param str String
+	 */
+	String(const char* str);
+
+	/**
+	 * \brief String constructor. Construct String from wchar_t*
+	 * \param str Wide string
+	 */
+	String(const wchar_t* str);
+
+	/**
+	 * \brief String constructor. Construct String from Char
+	 * \param ch Char
+	 */
+	String(const Char& ch);
+
+	/**
+	 * \brief String constructor. Construct String from uint32_t.
+	 * Character may be ansi char, wide char or curses char
+	 * \param ch Char
+	 */
+	String(uint32_t ch);
+
+    /**
+	 * \brief String constructor. Construct String from basic_string<Char>
+	 * \param data Vector of Char's
+	 */
+	String(const std::basic_string<Char>& data);
 
     /**
      * \brief Default String constructor
@@ -53,6 +83,63 @@ public:
      * \return Reference to \a char
      */
     Char& operator [](size_t index);
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of two strings.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const String& right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of two strings.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const std::string& right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of two strings.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const std::wstring& right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of two strings.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const char* right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of two strings.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const wchar_t* right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+	 * This operator returns the sum of string and char.
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(const Char& right) const;
+
+	/**
+	 * \brief Overload of the binary + operator.
+     * This operator returns the sum of string and char.
+	 * Character may be ansi char, wide char or curses char
+	 * \param right Right operand
+	 * \return Result of \a left + \a right
+	 */
+	String operator +(uint32_t right);
 
     /**
      * \brief Overload of the binary += operator.
@@ -86,29 +173,152 @@ public:
      */
     String& operator +=(const std::wstring& right);
 
-    /**
-     * \brief Overload of the binary += operator.
-     * This operator inserts char in back of this String
-     * \param right Right operand
-     * \return Reference to \a this
-     */
-    String& operator +=(const char& right);
+	/**
+	 * \brief Overload of the binary += operator.
+	 * This operator inserts ansi string in back of this String
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator +=(const char* right);
+
+	/**
+	 * \brief Overload of the binary += operator.
+	 * This operator inserts wide string in back of this String
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator +=(const wchar_t* right);
 
     /**
      * \brief Overload of the binary += operator.
-     * This operator inserts wchar in back of this String
+     * This operator inserts char in back of this String.
+	 * Character may be ansi char, wide char or curses char
      * \param right Right operand
      * \return Reference to \a this
      */
-    String& operator +=(const wchar_t& right);
+    String& operator +=(uint32_t right);
 
-    /**
-     * \brief Overload of the binary += operator.
-     * This operator inserts uint16_t (wchar) in back of this String
-     * \param right Right operand
-     * \return Reference to \a this
-     */
-    String& operator +=(const uint16_t& right);
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(const Char& right);
+
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(const std::string& right);
+
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(const std::wstring& right);
+
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(const char* right);
+
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(const wchar_t* right);
+
+	/**
+	 * \brief Overload of the binary = operator.
+	 * \param right Right operand
+	 * \return Reference to \a this
+	 */
+	String& operator =(uint32_t right);
+
+	/**
+	 * \brief Overload of the == operator
+	 * This operator compares two Strings and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are equal, false if they are different
+	 */
+	bool operator ==(const String& right) const;
+
+	/**
+	 * \brief Overload of the == operator
+	 * This operator compares String and std::string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are equal, false if they are different
+	 */
+	bool operator ==(const std::string& right) const;
+
+	/**
+	 * \brief Overload of the == operator
+	 * This operator compares String and std::wstring and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are equal, false if they are different
+	 */
+	bool operator ==(const std::wstring& right) const;
+
+	/**
+	 * \brief Overload of the == operator
+	 * This operator compares String and ansi string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are equal, false if they are different
+	 */
+	bool operator ==(const char* right) const;
+
+	/**
+	 * \brief Overload of the == operator
+	 * This operator compares String and wide string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are equal, false if they are different
+	 */
+	bool operator ==(const wchar_t* right) const;
+
+	/**
+	 * \brief Overload of the != operator
+	 * This operator compares two Strings and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are different, false if they are equal
+	 */
+	bool operator !=(const String& right) const;
+
+	/**
+	 * \brief Overload of the != operator
+	 * This operator compares String and std::string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are different, false if they are equal
+	 */
+	bool operator !=(const std::string& right) const;
+
+	/**
+	 * \brief Overload of the != operator
+	 * This operator compares String and std::wstring and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are different, false if they are equal
+	 */
+	bool operator !=(const std::wstring& right) const;
+
+	/**
+	 * \brief Overload of the != operator
+	 * This operator compares String and ansi string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are different, false if they are equal
+	 */
+	bool operator !=(const char* right) const;
+
+	/**
+	 * \brief Overload of the != operator
+	 * This operator compares String and wide string and check if they are equal.
+	 * \param right Right operand
+	 * \return True if strings are different, false if they are equal
+	 */
+	bool operator !=(const wchar_t* right) const;
 
     /**
      * \brief Default copy assignment operator
@@ -128,10 +338,22 @@ public:
     ~String() = default;
 
     /**
-     * \brief Get string for curses
-     * \return Curses string
+     * \brief Convert String to chtype* (it is equal uint32_t*) 
+     * \return uint32_t*
      */
     uint32_t* toCursesStr();
+
+    /**
+	 * \brief Convert String to std::string
+	 * \return std::string
+	 */
+	std::string toString() const;
+
+	/**
+	 * \brief Convert String to std::wstring
+	 * \return std::wstring
+	 */
+	std::wstring toWString() const;
 
     /**
      * \brief Get begin iterator
@@ -163,10 +385,16 @@ public:
      */
     size_t size() const;
 
+    /**
+	 * \brief Get raw data
+	 * \return Reference to data
+	 */
+	std::basic_string<Char>& getData();
+
 private:
     /**
      * \brief Data of string
      */
-    std::vector<Char> m_data;
+    std::basic_string<Char> m_data;
 };
 }
--- src/XCurses/Text/String.cpp
+++ src/XCurses/Text/String.cpp
@@ -1,13 +1,39 @@
 #include <XCurses/Text/String.h>
+#include <utility>
 
 namespace xcur {
 String::String(const std::string& str) :
-    m_data(str.begin(), str.end() + 1)
+    m_data(str.begin(), str.end())
 {
 }
 
 String::String(const std::wstring& str) :
-    m_data(str.begin(), str.end() + 1)
+	m_data(str.begin(), str.end())
+{
+}
+
+String::String(const char* str) :
+    String(std::string(str))
+{
+}
+
+String::String(const wchar_t* str) :
+	String(std::wstring(str))
+{
+}
+
+String::String(const Char& ch) :
+	m_data(1, ch)
+{
+}
+
+String::String(uint32_t ch) :
+	String(Char(ch))
+{
+}
+
+String::String(const std::basic_string<Char>& data) :
+    m_data(data)
 {
 }
 
@@ -16,59 +42,182 @@ Char& String::operator[](size_t index)
     return m_data[index];
 }
 
+String String::operator+(const String& right) const
+{
+	return String(m_data + right.m_data);
+}
+
+String String::operator+(const std::string& right) const
+{
+	return *this + String(right);
+}
+
+String String::operator+(const std::wstring& right) const
+{
+	return *this + String(right);
+}
+
+String String::operator+(const char* right) const
+{
+	return *this + String(right);
+}
+
+String String::operator+(const wchar_t* right) const
+{
+	return *this + String(right);
+}
+
+String String::operator+(const Char& right) const
+{
+	return String(m_data + right);
+}
+
+String String::operator+(uint32_t right)
+{
+	return *this + Char(right);
+}
+
 String& String::operator+=(const String& right)
 {
-    m_data.insert(m_data.end(), right.begin(), right.end());
-    return *this;
+	return *this = *this + right;
 }
 
 String& String::operator+=(const Char& right)
 {
-    if (right.getChar() != 0) {
-        m_data.push_back(right);
-    }
-    return *this;
+	return *this = *this + right;
 }
 
 String& String::operator+=(const std::string& right)
 {
-    m_data.insert(m_data.end(), right.begin(), right.end() + 1);
-    return *this;
+	return *this = *this + right;
 }
 
 String& String::operator+=(const std::wstring& right)
 {
-    m_data.insert(m_data.end(), right.begin(), right.end() + 1);
+	return *this = *this + right;
+}
+
+String& String::operator+=(const char* right)
+{
+	return *this = *this + right;
+}
+
+String& String::operator+=(const wchar_t* right)
+{
+	return *this = *this + right;
+}
+
+String& String::operator+=(uint32_t right)
+{
+	return *this = *this + right;
+}
+
+String& String::operator=(const Char& right)
+{
+	m_data = right;
     return *this;
 }
 
-String& String::operator+=(const char& right)
+String& String::operator=(const std::string& right)
 {
-    if (right != 0) {
-        m_data.emplace_back(right);
-    }
-    return *this;
+	return *this = String(right);
 }
 
-String& String::operator+=(const wchar_t& right)
+String& String::operator=(const std::wstring& right)
 {
-    if (right != 0) {
-        m_data.emplace_back(right);
-    }
-    return *this;
+	return *this = String(right);
 }
 
-String& String::operator+=(const uint16_t& right)
+String& String::operator=(const char* right)
 {
-    if (right != 0) {
-        m_data.emplace_back(right);
-    }
-    return *this;
+	return *this = String(right);
+}
+
+String& String::operator=(const wchar_t* right)
+{
+	return *this = String(right);
+}
+
+String& String::operator=(uint32_t right)
+{
+	return *this = Char(right);
+}
+
+bool String::operator==(const String& right) const
+{
+	return m_data == right.m_data;
+}
+
+bool String::operator==(const std::string& right) const
+{
+	return toString() == right;
+}
+
+bool String::operator==(const std::wstring& right) const
+{
+	return toWString() == right;
+}
+
+bool String::operator==(const char* right) const
+{
+	return toString() == right;
+}
+
+bool String::operator==(const wchar_t* right) const
+{
+	return toWString() == right;
+}
+
+bool String::operator!=(const String& right) const
+{
+	return !(*this == right);
+}
+
+bool String::operator!=(const std::string& right) const
+{
+	return !(*this == right);
+}
+
+bool String::operator!=(const std::wstring& right) const
+{
+	return !(*this == right);
+}
+
+bool String::operator!=(const char* right) const
+{
+	return !(*this == right);
+}
+
+bool String::operator!=(const wchar_t* right) const
+{
+	return !(*this == right);
 }
 
 uint32_t* String::toCursesStr()
 {
     return reinterpret_cast<uint32_t*>(m_data.data());
+}
+
+std::string String::toString() const
+{
+	std::string result(m_data.size(), '\0');
+	auto strIt = m_data.begin();
+	for (auto& ch : result) {
+		ch = static_cast<uint8_t>(strIt->toCursesChar());
+		++strIt;
+	}
+	return result;
+}
+
+std::wstring String::toWString() const
+{
+	std::wstring result(m_data.size(), L'\0');
+	auto strIt = m_data.begin();
+	for (auto& ch : result) {
+		ch = static_cast<uint16_t>(strIt->toCursesChar());
+		++strIt;
+	}
+	return result;
 }
 
 String::Iterator String::begin()
@@ -94,5 +243,10 @@ String::ConstIterator String::end() const
 size_t String::size() const
 {
     return m_data.size();
+}
+
+std::basic_string<Char>& String::getData()
+{
+	return m_data;
 }
 }
